ded5d61637fab33fe92e0bad1233385fa60ab27c
vinay chowdary <vinay@myasus>
2020-07-07 18:45:49 +0530

    merged calculateAVG.sh into Arithmetic.sh

--- practise/Arithmetic.sh
+++ practise/Arithmetic.sh
@@ -7,4 +7,19 @@
 read line;
 printf "%.3f" "$(echo "scale = 4; $line" | bc)"
 
+
+#calculating average
+
+read n
+NUM=0
+for (( i=0 ; i<n ; i++ ))
+do
+    read num[i]
+    NUM=$(( NUM + num[i] )) 
+done
+
+#echo $((NUM / n))
+
+printf "%0.3f" "$(echo "scale=4; $NUM / $n" | bc)"
+
 #end
