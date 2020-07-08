12cbbd1b47294b390026682345f2925da8cedfd2
vinay-chowdary <vinaychowdary.dev@gmail.com>
2020-07-08 17:09:30 +0530

    updated:functions.sh

--- practise/functions.sh
+++ practise/functions.sh
@@ -5,9 +5,14 @@ function print()
     echo "this is inside the function.Number of arguments passed are $#"
     echo "the arguments are $@"
     VAR="var inside function"
+    for i in $@
+    do
+        echo "$i"
+    done
+    local VAR2="accessed only inside a function"
 }
 
-#nothing will be printed
+#nothing will be printed it was not defined in global scope
 echo $VAR
 
 # declare same variable outside the function
@@ -15,3 +20,5 @@ VAR="var outside function"
 echo $VAR
 print abc def ghi
 echo $VAR
+
+# echo $VAR2 cannot be accessed outside function.defined as local variable
